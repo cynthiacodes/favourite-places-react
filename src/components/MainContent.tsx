e5a590d7a4e52df611f7f13413e6a0b2f2c968b7
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-29 17:11:24 +0100

    moved photos to public folder and added images link path from public folder

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,13 @@
 import PlaceEntry from "./PlaceEntry";
 
 
+
+
 function MainContent(): JSX.Element {
     return (
         <main>
         <PlaceEntry
-            imageURL = {"./assets/images/zanzibar.jpg"}
+            imageURL = {"/images/zanzibar.jpg"}
             imageText="sunset over the Indian Ocean in Zanzibar"
             title= {"Beautiful sunset at the beach"}
             placeName= {"Zanizibar"}
@@ -14,9 +16,9 @@ function MainContent(): JSX.Element {
             text = {"Nungwi in Zanzibar is a truly enchanting destination that combines natural beauty, underwater wonders, and a friendly atmosphere"}
         />
         <PlaceEntry
-            imageURL = {"./assets/images/giraffe.jpg"}
+            imageURL = {"/images/giraffe.jpg"}
             imageText= {"giraffe in the sunset"}
-            title= {"Beautiful sunset at the beach"}
+            title= {"Seeing Giraffes in their natural habitat"}
             placeName= {"Maasai Mara Safari"}
             countryName= {"Kenya"}
             mapURL= {"https://goo.gl/maps/b7gnfBtx2oG2PWsP8"}
@@ -24,7 +26,28 @@ function MainContent(): JSX.Element {
                 "As the golden sun begins to set over the vast plains of Maasai Mara, casting a warm glow across the landscape, you find yourself in awe of the breathtaking beauty that surrounds you."
             }
         />
-
+        <PlaceEntry
+            imageURL = {"/images/lyme-park.jpg"}
+            imageText= {"picnic at lyme park"}
+            title= {"Picnic at Lyme Park"}
+            placeName= {"Cheshire"}
+            countryName= {"England"}
+            mapURL= {"https://goo.gl/maps/tk1wNeDQx14xX13FA"}
+            text = {
+                "Lyme Park is an enchanting estate that seamlessly combines stunning landscapes, grand architecture, and a rich history."
+            }
+        />
+        <PlaceEntry
+            imageURL = {"/images/suzhou.jpg"}
+            imageText= {"walking through the Venice of China"}
+            title= {"walking through the Venice of China"}
+            placeName= {"Suzhou"}
+            countryName= {"China"}
+            mapURL= {"https://goo.gl/maps/sUaHa7TJcVmpcqk77"}
+            text = {
+                "Suzhou, often referred to as the \"Venice of China,\" is a city renowned for its enchanting waterways, classical gardens, and rich cultural heritage."
+            }
+        />
         </main>
 
     );
