1dfb518874c67ff536124a885b6a50e76bd74d1f
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-29 14:09:03 +0100

    added MainContent function to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import PageFooter from "./components/PageFooter";
+import MainContent from "./components/MainContent";
 import PageHeader from "./components/PageHeader";
+import PageFooter from "./components/PageFooter";
+
 
 function App(): JSX.Element {
   return (
     <>
      <PageHeader />
+     <MainContent />
      <PageFooter />
     
     </>
