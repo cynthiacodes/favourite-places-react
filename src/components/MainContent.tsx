2fad93cbdf6e593feb0b741a3e9014f35cf91f4d
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-29 15:03:35 +0100

    added second entry & added direct link to picture link for imageURL

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,11 @@
 import PlaceEntry from "./PlaceEntry";
-import ZanzibarImage from "./assets/images/zanzibar.jpg"
+
 
 function MainContent(): JSX.Element {
     return (
+        <main>
         <PlaceEntry
-            imageURL = {ZanzibarImage}
+            imageURL = {"./assets/images/zanzibar.jpg"}
             imageText="sunset over the Indian Ocean in Zanzibar"
             title= {"Beautiful sunset at the beach"}
             placeName= {"Zanizibar"}
@@ -12,7 +13,19 @@ function MainContent(): JSX.Element {
             mapURL= {"https://goo.gl/maps/YkieqGjkieoNo46Z9"}
             text = {"Nungwi in Zanzibar is a truly enchanting destination that combines natural beauty, underwater wonders, and a friendly atmosphere"}
         />
+        <PlaceEntry
+            imageURL = {"./assets/images/giraffe.jpg"}
+            imageText= {"giraffe in the sunset"}
+            title= {"Beautiful sunset at the beach"}
+            placeName= {"Maasai Mara Safari"}
+            countryName= {"Kenya"}
+            mapURL= {"https://goo.gl/maps/b7gnfBtx2oG2PWsP8"}
+            text = {
+                "As the golden sun begins to set over the vast plains of Maasai Mara, casting a warm glow across the landscape, you find yourself in awe of the breathtaking beauty that surrounds you."
+            }
+        />
 
+        </main>
 
     );
 }
