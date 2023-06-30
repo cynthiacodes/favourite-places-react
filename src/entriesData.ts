5a9fb7ae9c94445aca1cc7ff617be62ff338975a
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-30 17:52:15 +0100

    formatted entriesData

--- src/entriesData.ts
+++ src/entriesData.ts
@@ -1,13 +1,11 @@
-export const myEntries= [
+export const myEntries = [
   {
-    imageURL:  "/images/zanzibar.jpg" ,
+    imageURL: "/images/zanzibar.jpg",
     imageText: "sunset over the Indian Ocean in Zanzibar",
-    title:  "Beautiful sunset at the beach" ,
-    placeName: "Zanzibar" ,
+    title: "Beautiful sunset at the beach",
+    placeName: "Zanzibar",
     countryName: "Tanzania",
-    mapURL:  "https://goo.gl/maps/YkieqGjkieoNo46Z9" ,
-    text: 
-      "Nungwi in Zanzibar is a truly enchanting destination that combines natural beauty, underwater wonders, and a friendly atmosphere."
-    
-  }
+    mapURL: "https://goo.gl/maps/YkieqGjkieoNo46Z9",
+    text: "Nungwi in Zanzibar is a truly enchanting destination that combines natural beauty, underwater wonders, and a friendly atmosphere.",
+  },
 ];
